7a5a11fe0fb8ca9a71b7aace33f3e9774c4db893
Ricardo Kullock <ricardokullock@gmail.com>
2021-05-07 09:02:53 -0300

    Dados acumulados via dict

--- board.py
+++ board.py
@@ -8,11 +8,12 @@ import base64
 import pandas as pd
 import io
 import datetime
-import plotly.express as px
+import plotly.graph_objects as go
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SKETCHY])
 
 app.layout = html.Div([
+    dcc.Store(id='store'),
     dbc.Row(
         dbc.Col([
             dcc.Upload([html.Div([
@@ -47,27 +48,38 @@ def parse_contents(contents):
         ])
     return df
 
-@app.callback(Output('grafico', 'figure'),
-              Input('upload', 'contents'))
-def upload(cont):
+@app.callback(Output('store', 'data'),
+              Input('upload', 'contents'),
+              State('store', 'data'))
+def my_data(cont, dat):
     if cont is not None:
         conteudos = [parse_contents(c) for c in cont]
-        tot = pd.concat(conteudos, ignore_index=True)
-        grafico = px.histogram(tot['Total'])
+        par = pd.concat(conteudos, ignore_index= True).to_dict('list')
+        if dat is not None:
+            tot = {i : dat[i]+par[i] for i in dat.keys()}
+            return tot
+        else:
+            return par
+    else:
+        return dash.no_update
+
+@app.callback(Output('grafico', 'figure'),
+              Input('store', 'data'))
+def upload(dat):
+    if dat is not None:
+        grafico = go.Figure(data=[go.Histogram(x=dat['Total'])])
         return grafico
     else:
         return dash.no_update
 
 @app.callback(Output('tab', 'children'),
-              Input('upload', 'contents'))
-def update_output(cont):
-    if cont is not None:
-        conteudos = [parse_contents(c) for c in cont]
-        tot = pd.concat(conteudos, ignore_index=True)
-        tabela = dash_table.DataTable(
-            data=tot.to_dict('records'),
-            columns=[{'name': i, 'id': i} for i in tot.columns])
-        return html.Div([tabela])
+              Input('store', 'data'))
+def update_output(dat):
+    if dat is not None:
+        df_par=pd.DataFrame.from_dict(dat)
+        tabela = dbc.Table.from_dataframe(
+                df_par, striped=True, bordered=True, hover=True)
+        return html.Div([tabela], style={'height': '450px', 'overflowY': 'auto'})
     else:
         return dash.no_update
 
